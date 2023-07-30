6920f6cbbd85f478799052fc2c9460e096c3f651
V <vladgrammer@yandex.ru>
2023-07-31 08:06:50 +1000

    Remote ch_2

--- ch_2/target/debug/ch_2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Automatica\Documents\code\project\rust-book\ch_2\target\debug\ch_2.exe: C:\Users\Automatica\Documents\code\project\rust-book\ch_2\src\main.rs
--- ch_2/target/debug/deps/cfg_if-dd55df008f24fc86.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Automatica\Documents\code\project\rust-book\ch_2\target\debug\deps\cfg_if-dd55df008f24fc86.rmeta: C:\Users\Automatica\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Automatica\Documents\code\project\rust-book\ch_2\target\debug\deps\libcfg_if-dd55df008f24fc86.rlib: C:\Users\Automatica\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Automatica\Documents\code\project\rust-book\ch_2\target\debug\deps\cfg_if-dd55df008f24fc86.d: C:\Users\Automatica\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Automatica\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
